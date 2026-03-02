82b8f60674018fb7b4a87fc7aedf070f7f740cf1
Nithin Godugu <nithingodugu24@gmail.com>
2026-03-02 17:55:34 +0530

    feat: store order inventory reservations

---
+++ inventory-service/src/main/java/com/nithingodugu/ecommerce/inventoryservice/domain/entity/InventoryReservation.java
@@ -0,0 +1,39 @@
+package com.nithingodugu.ecommerce.inventoryservice.domain.entity;
+
+import com.nithingodugu.ecommerce.inventoryservice.domain.entity.enums.ReservationStatus;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Table(name = "inventory_reservations")
+public class InventoryReservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long orderId;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+}
