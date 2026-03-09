046a9f6d8e260ec3a9c5485cc94ab5baf4b7c0e0
Nithin Godugu <nithingodugu24@gmail.com>
2026-03-09 19:43:04 +0530

    chore: admin data init from data.sql

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -1,2 +1,10 @@
--- INSERT INTO users (id,created_at,email,password_hash,role,status) VALUES
---     ('41de4b06-35ec-42d0-958a-def1831d3a06','2026-02-15 14:46:10.114','admin@gmail.com','$2a$10$C1HWR9nBDdSO2zMmAq9ieujnF9Iab0xNL0eFihQaxXaG6xbpbgF8i','ADMIN','ACTIVE');
+INSERT INTO users (id, created_at, email, password_hash, role, status)
+VALUES (
+    '41de4b06-35ec-42d0-958a-def1831d3a06',
+        NOW(),
+        'admin@gmail.com',
+        '$2a$10$C1HWR9nBDdSO2zMmAq9ieujnF9Iab0xNL0eFihQaxXaG6xbpbgF8i',
+        'ADMIN',
+        'ACTIVE'
+    )
+ON CONFLICT (email) DO NOTHING;
