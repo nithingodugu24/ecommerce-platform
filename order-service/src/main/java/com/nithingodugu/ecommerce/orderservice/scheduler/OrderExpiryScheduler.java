2f4c038e9ab327d1fd02c25c888c6a3f9db6f50b
Nithin Godugu <nithingodugu24@gmail.com>
2026-03-20 21:39:56 +0530

    feat: logging and tracing for kafka in orderservice

--- order-service/src/main/java/com/nithingodugu/ecommerce/orderservice/scheduler/OrderExpiryScheduler.java
+++ order-service/src/main/java/com/nithingodugu/ecommerce/orderservice/scheduler/OrderExpiryScheduler.java
@@ -6,6 +6,7 @@ import com.nithingodugu.ecommerce.orderservice.repository.OrderRepository;
 import com.nithingodugu.ecommerce.orderservice.service.OrderService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrderExpiryScheduler {
