26946ae338ee1e631ad1667efcd98bdd307f324f
Nithin Godugu <nithingodugu24@gmail.com>
2026-03-20 21:33:55 +0530

    feat: logging & tracing setup in orderservice

--- order-service/src/main/java/com/nithingodugu/ecommerce/orderservice/security/HeaderAuthenticationFilter.java
+++ order-service/src/main/java/com/nithingodugu/ecommerce/orderservice/security/HeaderAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -31,21 +32,27 @@ public class HeaderAuthenticationFilter extends OncePerRequestFilter {
         String userRole = request.getHeader("X-USER-ROLE");
 
         if (userId != null && userRole != null){
-            List<GrantedAuthority> authorities= List.of(
+            List<GrantedAuthority> authorities = List.of(
                     new SimpleGrantedAuthority("ROLE_" + userRole)
             );
 
-           UsernamePasswordAuthenticationToken auth =
-                   new UsernamePasswordAuthenticationToken(
-                           userId,
-                           null,
-                           authorities
-                   );
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(
+                            userId,
+                            null,
+                            authorities
+                    );
 
-           SecurityContextHolder.getContext().setAuthentication(auth);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            MDC.put("userId", userId);
+            MDC.put("userRole", userRole);
         }
 
         filterChain.doFilter(request, response);
+
+        MDC.remove("userId");
+        MDC.remove("userRole");
 
     }
 }
